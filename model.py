4671abe4e5cc8ef98a7a3504a617552cb437cf6a
Alireza <alirzahosinopoor.1390@gmail.com>
2025-01-10 11:32:08 +0330

    add lazy loading

--- model.py
+++ model.py
@@ -1,39 +1,49 @@
-import os
-from transformers import GPT2LMHeadModel, GPT2Tokenizer, CodeGenForCausalLM, AutoTokenizer
+import torch
+import gc
+from transformers import AutoModelForCausalLM, AutoTokenizer, GPT2LMHeadModel, GPT2Tokenizer
 
-def load_model(model_name):
-    """
-    Load the model and tokenizer based on the model_name.
-    Supports gpt2, gpt2-medium, gpt2-medium-persian
-    """
-    if model_name == "gpt2":
-        print("Loading GPT-2 base model...")
-        model = GPT2LMHeadModel.from_pretrained("./models/gpt2")
-        tokenizer = GPT2Tokenizer.from_pretrained("./models/gpt2")
-    elif model_name == "gpt2-medium":
-        print("Loading GPT-2 medium model...")
-        model = GPT2LMHeadModel.from_pretrained("./models/gpt2-medium")
-        tokenizer = GPT2Tokenizer.from_pretrained("./models/gpt2-medium")
-    elif model_name == "gpt2-persian":
-        print("Loading GPT-2 Persian model...")
-        model = GPT2LMHeadModel.from_pretrained("./models/gpt2-medium-persian")
-        tokenizer = GPT2Tokenizer.from_pretrained("./models/gpt2-medium-persian")
-    elif model_name == "codegen":
-        print("Loading CodeGen model...")
-        model = CodeGenForCausalLM.from_pretrained("./models/codegen", device_map='cpu')
-        tokenizer = AutoTokenizer.from_pretrained("./models/codegen")
-    elif model_name == "gpt2-large":
-        print("Loading GPT2 Large Model...")
-        if os.path.exists(os.path.join("./models/gpt2-large", "pytorch_model.bin")):
-            model = GPT2LMHeadModel.from_pretrained("./models/gpt2-large")
-        elif os.path.exists(os.path.join("./models/gpt2-large", "model.safetensors")):
-            model = GPT2LMHeadModel.from_pretrained("./models/gpt2-large", from_tf=False, trust_remote_code=True)
-        else:
-            raise FileNotFoundError("Neither 'pytorch_model.bin' nor 'model.safetensors' found in the specified path.")      
-        tokenizer = GPT2Tokenizer.from_pretrained("./models/gpt2-large") # Load Tokenizer
+# Dictionary of models and paths
+model_dict = {
+    "gpt2": {"path": "./models/gpt2", "library": GPT2LMHeadModel, "tokenizer": GPT2Tokenizer},
+    "gpt2-medium": {"path": "./models/gpt2-medium", "library": GPT2LMHeadModel, "tokenizer": GPT2Tokenizer},
+    "gpt2-large": {"path": "./models/gpt2-large", "library": AutoModelForCausalLM, "tokenizer": AutoTokenizer},
+    "gpt2-persian": {"path": "./models/gpt2-medium-persian", "library": GPT2LMHeadModel, "tokenizer": AutoTokenizer},
+    "codegen": {"path": "./models/codegen", "library": AutoModelForCausalLM, "tokenizer": AutoTokenizer},
+}
 
-    # Configure the pad_token for text models
-    if tokenizer:
+loaded_models = {}
+
+def load_model_lazy(model_name):
+    """
+    Lazy loading of the model
+    """
+    if not isinstance(model_name, str):
+        raise ValueError(f"Model name must be a string, not {type(model_name)}")
+    if model_name not in model_dict:
+        raise ValueError(f"Model {model_name} not found!")
+
+    model_info = model_dict[model_name]
+    print(f"Loading model: {model_name}")
+
+    # Load model and tokenizer
+    model = model_info["library"].from_pretrained(model_info["path"])
+    tokenizer = model_info["tokenizer"].from_pretrained(model_info["path"])
+
+    # Default settings
+    if tokenizer.pad_token is None:
         tokenizer.pad_token = tokenizer.eos_token
     
-    return model, tokenizer
+    loaded_models[model_name] = {"model": model, "tokenizer": tokenizer}
+
+    return {"model": model, "tokenizer": tokenizer}
+
+
+def unload_model(model_name):
+    global loaded_models
+    if model_name in loaded_models:
+        del loaded_models[model_name]
+        torch.cuda.empty_cache()
+        gc.collect()
+        print(f"Model {model_name} unloaded and memory cleared.")
+    else:
+        print(f"Model {model_name} was not loaded.")
